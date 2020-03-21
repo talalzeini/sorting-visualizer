c2dc9402cb9b3107185972240e52ecb69564b975
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-03-21 15:01:21 -0700

    added design, selection sort and buttons

--- script.js
+++ script.js
@@ -1,53 +1,170 @@
+// Set Global Variables 
 
-var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const number = document.getElementById('numbers')
-var arraySize = 11;
+let values = []; 
+let w = 50;
 
 
-function selectionSort(array, arraySize){
-    // adding + 1 to to index of i when going through all the array is finished
-    for(i = 0; i < arraySize-1; i++){
-        var counter = 0;
-        var i;
-        var j;
-        var small;
-        var temp;
-        small = i;
-        // showing changes
-        while (counter < arraySize-1){
-        
-            number.innerText += "   ";
-            number.innerText += ( ' ' +  array[counter++] ) ;
 
+// To store the state of each bar 
+// in order to change the color 
+let states = []; 
+let width = 1200
+let height = 600
+
+function setup() { 
     
-        }
-        number.innerHTML += "<br><br>";
-                    // j is always ahead of i by 1 index
-                    for(j=i+1; j < arraySize; j++){
-                            
-                            // Comparing value of j and value of small
-                            if(array[j] < array[small])
-                                // small takes the index of j
-                                small = j;
-                          
-                            }
-                            // switching value of i and value of small
-                            temp = array[i];
-                            array[i] = array[small];
-                            array[small] = temp;
+    // Create Canvas of Size Windows 
+    // Width * Windows Height 
+    createCanvas(width, height); 
 
 
-                    }   
-number.disabled = true;
-                }
+    // Insert Random values in array 
+    values = new Array(floor(width/w)); 
 
-function Start(){
-
-
-    var size = 10;
-    var  myArray = [size];
-    for(var number=0; number<size; number++){ 
-            myArray[number] = Math.floor(Math.random() * 100) + 1 
+    for(let i = 0; i < values.length; i++) { 
+        values[i] = float(random(height+5)); 
+    states[i] = 2
+    if(states[i] == 2){
+        fill(255, 173, 153);
     }
-    selectionSort(myArray, size);
+    } 
+
+    document.getElementById('bubble').disabled = false;
+    document.getElementById('selection').disabled = false;
+   
+    // Call to bubble sort function 
+   
+} 
+
+// Definition of bubble sort 
+async function selectionSort(arr, start, end) { 
+
+    var small;
+    var i;
+    states[i] = 1;
+
+    for( i = 0; i < arr.length; i++) { 
+
+            small = i;	
+            
+        
+        
+
+        for(var j = i+1; j < arr.length; j++) { 
+
+            if(arr[j] < arr[small]) { 
+
+                states[small] = 2;
+                small = j;
+
+            }
+           
+
+        }
+
+        states[i] = 0;
+                // Call to swap function 
+                await swap(arr, small, i); 
+            
+        } 
+        document.getElementById('resetButton').disabled = false;
+        
+    return arr; 
+
+} 
+async function bubbleSort(arr, start, end) { 
+    if(start >= end) { 
+        return; 
+    } 
+    
+    for(var i = 0; i < end; i++) { 
+        for(var j = 0; j < end-i; j++) { 
+            if(arr[j] >= arr[j+1]) { 
+               
+   
+                // Call to swap function 
+                await swap(arr, j, j+1); 
+              
+            } 
+
+        } 
+        states[i] = 0;
+    } 
+    document.getElementById('resetButton').disabled = false;
+    return arr; 
+} 
+   
+
+// Definition of draw function 
+function draw() { 
+    background(0); 
+    
+    for(let i = 0; i < values.length; i++) { 
+      
+        fill(255, 173, 153); // red
+        
+        if(states[i] == 0) { 
+            fill(128, 255, 170); // green
+        } 
+        else if (states[i] == 1) { 
+            
+            // Element currently sorting 
+            fill("#58FA82"); 
+        } 
+        else if (states[i] == 2) { 
+            
+            // Element currently sorting 
+           fill(255, 173, 153);
+        } 
+  
+        
+    
+
+        else { 
+            fill(255, 173, 153); // red
+        }  
+        rect(i*w, height - values[i], w, values[i]); 
+    } 
+}
+
+// Definition of swap function 
+async function swap(arr, a, b) { 
+    
+    await sleep(100); 
+    let t = arr[a]; 
+    arr[a] = arr[b]; 
+    arr[b] = t; 
+} 
+
+// Definition of sleep function 
+function sleep(ms) { 
+    return new Promise(resolve => setTimeout(resolve, ms)); 
+} 
+
+
+function selection()
+{
+    document.getElementById('bubble').disabled = true;
+    document.getElementById('resetButton').disabled = true;
+    selectionSort(values, 0, values.length); 
+
+}
+function bubble()
+{
+    document.getElementById('selection').disabled = true;
+    document.getElementById('resetButton').disabled = true;
+    bubbleSort(values, 0, values.length); 
+ 
+
+}
+
+function search(ele) {
+
+    if(event.key === 'Enter') {
+            
+        eh = ele.value;
+
+       
+
+}
 }
