c5bb69df2d96038056d319dd2bdac5d1a6c675dc
talalzeini <talalelzeini@gmail.com>
2023-02-10 23:38:03 -0800

    made a major design change

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,151 +1,66 @@
-//
-//
-//
-//
-//
-//
-//
-//
-// range speed function starts here
-// range speed function starts here
+const rangeWidth = document.getElementById("rangeWidth");
+const resetButton = document.getElementById("resetButton");
+const rangeToDisable = document.getElementById("rangeToDisable");
+syncIcon2;
+
 function rangeSpeed(fast) {
   speed = fast;
 }
-// range speed function ends here
-// range speed function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// range slide function starts here
-// range slide function starts here
+
 function rangeSlide(value) {
   w = value;
   setup();
 }
-// range slide function ends here
-// range slide function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// swap function starts here
-// swap function starts here
+
 async function swap(arr, a, b) {
   await sleep(speed);
   let t = arr[a];
   arr[a] = arr[b];
   arr[b] = t;
 }
-// swap function ends here
-// swap function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// speed function starts here
-// speed function starts here
+
 function sleep(speed) {
   return new Promise((resolve) => setTimeout(resolve, speed));
 }
-// speed function ends here
-// speed function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// enable function starts here
-// enable function starts here
+
 function enable() {
-  rangeWidth.disabled = false;
-  resetButton.disabled = false;
-  resetButton.classList.remove("disabledButton");
-  resetButton.classList.add("reset");
-  rangeToDisable.classList.add("white");
-  rangeToDisable.classList.remove("gray");
   var buttons = document.getElementsByClassName("buttons");
+  var resetButtons = document.getElementsByClassName("reset");
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].disabled = false;
     buttons[i].classList.remove("disabledButton");
     buttons[i].classList.add("sort");
   }
+  for (var i = 0; i < resetButtons.length; i++) {
+    resetButtons[i].disabled = false;
+    resetButtons[i].classList.remove("disabledButton");
+    resetButtons[i].classList.add("sort");
+  }
 }
-// enable function ends here
-// enable function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// disable function starts here
-// disable function starts here
-function disable() {
-  rangeWidth.disabled = true;
-  resetButton.disabled = true;
-  resetButton.classList.add("disabledButton");
-  resetButton.classList.remove("reset");
-  rangeToDisable.classList.remove("white");
-  rangeToDisable.classList.add("gray");
 
+function disable() {
   var buttons = document.getElementsByClassName("buttons");
+  var resetButtons = document.getElementsByClassName("reset");
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].disabled = true;
     buttons[i].classList.add("disabledButton");
     buttons[i].classList.remove("sort");
   }
+  for (var i = 0; i < resetButtons.length; i++) {
+    resetButtons[i].disabled = true;
+    resetButtons[i].classList.add("disabledButton");
+    resetButtons[i].classList.remove("sort");
+  }
 }
-// disable function ends here
-// disable function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// rotate button function starts here
-// rotate button function starts here
+
 function rotateButton() {
   document.getElementById("defaultCanvas0").classList.add("canvasRotation");
   document.getElementById("syncIcon").classList.add("hide");
   document.getElementById("syncIcon2").classList.remove("hide");
 }
-// rotate button function ends here
-// rotate button function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// rotate back function starts here
-// rotate back function starts here
+
 function rotateBack() {
   document.getElementById("defaultCanvas0").classList.remove("canvasRotation");
   document.getElementById("syncIcon2").classList.add("hide");
   document.getElementById("syncIcon").classList.remove("hide");
 }
-// rotate back function ends here
-// rotate back function ends here
--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,44 @@
-var bubble = document.getElementById("bubbleID");
-var selection = document.getElementById("selectionID");
-var insertion = document.getElementById("insertionID");
-var rangeWidth = document.getElementById("rangeWidth");
-var resetButton = document.getElementById("resetButton");
-var rangeToDisable = document.getElementById("rangeToDisable");
+const w = 10;
+const speed = 10;
+const bubble = document.getElementById("bubbleID");
+const selection = document.getElementById("selectionID");
+const insertion = document.getElementById("insertionID");
+const backgroundColorString = "rgb(110, 197, 255)";
 let values = [];
 let states = [];
-let w = 10;
-let speed = 10;
-//
-//
-//
-//
-//
-//
-//
-//
-// setup function starts here
-// setup function starts here
+
 function setup() {
   stopWatch();
   createCanvas(
-    displayWidthSize(1200),
-    displayHeightSize(displayWidthSize($(window).width() - 150) / 2)
+    displayWidthSize($(window).width()),
+    displayHeightSize(
+      displayWidthSize($(window).width() - $(window).width() / 3) / 2
+    )
   );
-  document.getElementById("defaultCanvas0").style.width = "95%";
+  document.getElementById("defaultCanvas0").style.width = "75%";
+  document
+    .getElementById("defaultCanvas0")
+    .classList.add("canvasNormalRotation");
   colorMode(HSB, height);
-  // Insert Random values in array
   values = new Array(floor(width / w) + 1);
+  console.log(values);
   for (let i = 0; i < values.length; i++) {
-    values[i] = float(random(height) + 20);
+    value = float(random(height) + 20);
+    while (value > height) {
+      value = float(random(height) + 20);
+    }
+    values[i] = value;
   }
   enable();
 }
-// setup function ends here
-// setup function ends here
-//
-//
-//
-//
-//
-//
-//
-//
-// draw function starts here
-// draw function starts here
+
 function draw() {
-  background(0);
+  background(backgroundColorString);
   for (let i = 0; i < values.length - 1; i++) {
-    let col = color(values[i], height, height);
-    stroke(255, 204, 0);
+    let bar = color(values[i], height, height);
+    stroke(0, 0, 0);
     strokeWeight(1);
-    fill(col);
+    fill(bar);
     rect(i * w, height - values[i], w, values[i]);
   }
 }
-// draw function ends here
-// draw function ends here
